1483abf61717bf368b70f93a528e624777a84621
markswinimer <markswinimer@gmail.com>
2017-10-17 11:59:13 -0400

    Added Restaurant model and tests and implemented suggested revisions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,18 +1,10 @@
 class Restaurant < ApplicationRecord
-  
-validates :name, presence: true
-validates :address, presence: true
-validates :city, presence: true,
-          numericality: false
 
-validates :state, presence: true
-validates :zip, presence: true,
-          numericality: true,
-          length: {is: 5}
-
-validates :dollar_value, presence: true,
-          inclusion: { in: ["$","$$","$$$"]}
-
-validates :phone_number, numericality: true,
-          length: {is: 10}
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true, numericality: false
+  validates :state, presence: true
+  validates :zip, presence: true, numericality: true, length: {is: 5}
+  validates :dollar_value, presence: true, inclusion: { in: [1, 2, 3] }
+  validates :phone_number, numericality: true, length: {is: 10}
 end
