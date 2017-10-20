659152a242b369bc3b3f7f7088b350f7b6f1bc12
devlin-person <devylin.91@gmail.com>
2017-10-20 14:09:47 -0400

    Wrote tests for restaurant/form components and containers.

---
+++ app/javascript/react/src/containers/RestaurantsContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import RestaurantFormContainer from './RestaurantFormContainer'
+
+class RestaurantsContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      restaurants: []
+    }
+
+    this.addNewRestaurant = this.addNewRestaurant.bind(this);
+  }
+
+  addNewRestaurant(formPayLoad) {
+    debugger;
+    fetch('/api/v1/restaurants', {
+    method: 'POST',
+    body: JSON.stringify(formPayLoad)
+    })
+  .then(response => response.json())
+  .then(responseData => {
+    this.setState({restaurants: [...this.state.restaurants, responseData]})
+    })
+  }
+  render() {
+    let addNewRestaurant = (formPayLoad) => this.addNewRestaurant(formPayLoad)
+
+    return(
+      <div className='container'>
+        <RestaurantFormContainer
+          addNewRestaurant={addNewRestaurant}
+        />
+      </div>
+    )
+  }
+}
+
+
+export default RestaurantsContainer
