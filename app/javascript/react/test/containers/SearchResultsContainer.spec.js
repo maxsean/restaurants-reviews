68ed372beb0092e66cbb624b1b60b20ea7936272
markswinimer <markswinimer@gmail.com>
2017-10-20 14:22:41 -0400

    added react searchbar, reformatted react containers and components, added more enzyme tests, fixed backend api endpoint for search controller.

---
+++ app/javascript/react/test/containers/SearchResultsContainer.spec.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
+import SearchResultsContainer from '../../src/containers/SearchResultsContainer'
+import RestaurantTile from '../../src/components/RestaurantTile'
+
+describe('A test for SearchResultsContainer', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const example = [
+      {name: "Panera", address: "55 Summer St.", city: "Boston",
+              state: "MA", zip: "02111", dollar_value: 2, user_id: 1},
+      {name: "Danera", address: "85 Summer St.", city: "Cambridge",
+              state: "MA", zip: "02121", dollar_value: 5, user_id: 2}
+    ]
+
+    wrapper = mount(<SearchResultsContainer restaurants={example}/>)
+  })
+
+  it('renders RestaurantTile components', () => {
+    expect(wrapper.find('RestaurantTile')).toBePresent();
+  })
+
+  it('a RestaurantTile component have specified props', () => {
+    expect(wrapper.find('RestaurantTile').at(0).props()).toEqual({
+      name: "Panera",
+      address: "55 Summer St.",
+      city: "Boston",
+      state: "MA",
+      zip: "02111",
+      dollar_value: 2,
+      description: undefined,
+	    picture: undefined,
+	    phone_number: undefined,
+	    website_url: undefined,
+	    dining_type: undefined,
+	    food_type: undefined
+    });
+  })
+
+  it('a RestaurantTile component have specified props', () => {
+    expect(wrapper.find('RestaurantTile').at(1).props()).toEqual({
+      name: "Danera",
+      address: "85 Summer St.",
+      city: "Cambridge",
+      state: "MA",
+      zip: "02121",
+      dollar_value: 5,
+      description: undefined,
+	    picture: undefined,
+	    phone_number: undefined,
+	    website_url: undefined,
+	    dining_type: undefined,
+	    food_type: undefined
+    })
+  })
+})
