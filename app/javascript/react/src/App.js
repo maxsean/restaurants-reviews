12b75b094f191999963435a3e0c8a6e8dcde757c
markswinimer <markswinimer@gmail.com>
2017-10-21 11:45:12 -0400

    Commit

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,22 +1,3 @@
-import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import SearchBarContainer from './containers/searchBarContainer'
-=======
-import WelcomeContainer from './containers/WelcomeContainer'
->>>>>>> f7924bccc85467d71bc4d717ed7f4b556e2a8417
-
-const App = props => {
-
-  return(
-    <div>
-<<<<<<< HEAD
-      <h1>Hello World</h1>
-      <SearchBarContainer />
-=======
-      <WelcomeContainer />
->>>>>>> f7924bccc85467d71bc4d717ed7f4b556e2a8417
-=======
 import { Route, IndexRoute, Router, browserHistory} from 'react-router';
 import WelcomeContainer from './containers/WelcomeContainer';
 import RestaurantsContainer from './containers/RestaurantsContainer';
@@ -31,7 +12,6 @@ const App = (props) => {
             component={RestaurantsContainer}/>
         </Route>
       </Router>
->>>>>>> 18e14bf8ee2887eaee5dd246354f6f74af5d7a0a
     </div>
   )
 }
