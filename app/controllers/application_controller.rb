d003596162d6006aa0ef3f61f76d8030e8f9fb1c
zhangyi.olivia@gmail.com <oliviazhang@MacBook-Pro.local>
2017-10-23 13:16:00 -0400

    Start carrierwave test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
  protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :avatar, :avatar_cache, :remove_avatar])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :password, :password_confirmation, :avatar, :avatar_cache, :remove_avatar])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :password, :password_confirmation, :avatar, :avatar_cache, :remove_avatar])
  end
 
 end
