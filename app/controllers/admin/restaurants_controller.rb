3e12f31e57e9c0ba17ebb098599c01853b074cff
zhangyi.olivia@gmail.com <oliviazhang@MacBook-Pro.local>
2017-10-25 09:47:05 -0400

    Add controllers and views for admin role

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,15 @@
 class Admin::RestaurantsController < AdminController
   def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
   end
 end
--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,2 +1,15 @@
 class Admin::ReviewsController < AdminController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to admin_reviews_path
+  end
 end
