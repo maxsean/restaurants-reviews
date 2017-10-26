41d7703a3325f1a37433881c1b4450ca88f8cda0
zhangyi.olivia@gmail.com <oliviazhang@MacBook-Pro.local>
2017-10-26 09:27:07 -0400

    Add admin role and fixed conflicts in react

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    binding.pry
     @user = User.find(params[:id])
   end
 end
--- app/javascript/react/src/containers/RestaurantsContainer.js
+++ app/javascript/react/src/containers/RestaurantsContainer.js
@@ -6,10 +6,22 @@ class RestaurantsContainer extends Component {
     super(props);
 
     this.state = {
-      restaurants: []
+      restaurant: {},
+      current_user: {}
     }
-
     this.addNewRestaurant = this.addNewRestaurant.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/users.json', {
+      credentials: 'same-origin',
+      method: "GET",
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({current_user: data.user})
+    })
   }
 
   addNewRestaurant(formPayLoad) {
@@ -19,9 +31,14 @@ class RestaurantsContainer extends Component {
     })
   .then(response => response.json())
   .then(responseData => {
-    this.setState({restaurants: [...this.state.restaurants, responseData]})
+    this.setState({restaurant: responseData})
     })
+    if (this.state.restaurant !== {}) {
+      window.location.href="/"
+      // window.location.href=`/restaurants/${this.state.restaurant.id}`
+    }
   }
+
   render() {
     let addNewRestaurant = (formPayLoad) => this.addNewRestaurant(formPayLoad)
 
@@ -29,11 +46,11 @@ class RestaurantsContainer extends Component {
       <div className='formContainer'>
         <RestaurantFormContainer
           addNewRestaurant={addNewRestaurant}
+          current_user={this.state.current_user}
         />
       </div>
     )
   }
 }
-
 
 export default RestaurantsContainer
